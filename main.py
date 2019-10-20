227c47a7920c92ba7fd4c1d6c437f620e424b3d2
Mark Cottman-Fields <cofiem@gmail.com>
2019-10-20 19:40:50 +1000

    Added initial python file structure and some placeholders.

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import escape
+
+# see https://cloud.google.com/functions/docs/writing/#structuring_source_code
+
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
---
+++ triplej/triplej_most_played.py
@@ -0,0 +1,1 @@
+# https://music.abcradio.net.au/api/v1/recordings/plays.json?order=desc&limit=50&service=triplej&from=2019-10-12T13:00:00Z&to=2019-10-19T13:00:00Z
